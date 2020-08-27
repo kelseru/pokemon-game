70ccbb8e2f75a771e2073bce47a4ad9965da7944
Bex Birkett <bex.birkett@outlook.com>
2020-08-27 15:12:52 +0100

    Updates

---
+++ precache-manifest.824941a531c7efd50cf0e28739901e83.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbce998077354c90adc206051932c232",
+    "url": "/pokemon-game/index.html"
+  },
+  {
+    "revision": "d60fb5dc8df1b41c84ec",
+    "url": "/pokemon-game/static/css/main.a351fa7c.chunk.css"
+  },
+  {
+    "revision": "689041c8e7f348ed375c",
+    "url": "/pokemon-game/static/js/2.8e265e7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-game/static/js/2.8e265e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60fb5dc8df1b41c84ec",
+    "url": "/pokemon-game/static/js/main.84ac5265.chunk.js"
+  },
+  {
+    "revision": "6d869adefd240c731b28",
+    "url": "/pokemon-game/static/js/runtime-main.61f3c20b.js"
+  },
+  {
+    "revision": "bee6d7e040703243ac88a01b14118c75",
+    "url": "/pokemon-game/static/media/gba-off.bee6d7e0.png"
+  },
+  {
+    "revision": "40c17b58999a03b2caa6ffdadf869504",
+    "url": "/pokemon-game/static/media/gba-on.40c17b58.png"
+  },
+  {
+    "revision": "11127666f925c45e1b525d229d7cab62",
+    "url": "/pokemon-game/static/media/pokeball.11127666.png"
+  },
+  {
+    "revision": "da578b76d9ee2d0746d258a442bfea0a",
+    "url": "/pokemon-game/static/media/title.da578b76.gif"
+  }
+]);
